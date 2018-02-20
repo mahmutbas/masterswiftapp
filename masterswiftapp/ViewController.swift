4ff602b2ec59aedeae250f384be63f37a51e8df4
Mahmut Bas <mahmut@tornado.local>
2018-02-20 11:39:47 +0300

    Initial Commit

---
+++ masterswiftapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  masterswiftapp
+//
+//  Created by Mahmut Bas on 20/02/2018.
+//  Copyright © 2018 Mahmut Bas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
