6053ef4d85135e127b9cb976f04e979b0bf12ac2
mahmutbas <mahmut@tornado.local>
2018-02-20 23:17:38 +0300

    Select image from library and set image to image view

--- masterswiftapp/ViewController.swift
+++ masterswiftapp/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var imgView: UIImageView!
+    
+    @IBAction func btnImportImage(_ sender: Any) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        imagePickerController.allowsEditing = false
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        {
+            imgView.image = image
+        }else
+        {
+            print("There was a problem getting the image")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
 
