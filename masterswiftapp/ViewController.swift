8921bb32d863d26832db6055050115b10c0ab123
mahmutbas <mahmut@tornado.local>
2018-02-21 14:24:34 +0300

    Open spinner and block all interactions from another buttons. Then allow interactions, hide spinner.

--- masterswiftapp/ViewController.swift
+++ masterswiftapp/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    var activityIndicator = UIActivityIndicatorView()
+    
+    @IBAction func pauseApp(_ sender: Any) {
+        activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+        // deny all interaction from view
+        UIApplication.shared.beginIgnoringInteractionEvents()
+    }
+    
+    @IBAction func resotreApp(_ sender: Any) {
+        activityIndicator.stopAnimating()
+        //allow all interactions from view
+        UIApplication.shared.endIgnoringInteractionEvents()
+    }
 }
 
