6621ae0338e01b3782f1c65302b8b40664da2271
mahmutbas <mahmut@tornado.local>
2018-02-23 10:37:49 +0300

    start Timer, call an method and stop timer

--- masterswiftapp/ViewController.swift
+++ masterswiftapp/ViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    var timer = Timer()
+    @IBAction func startTimer(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.processTimer), userInfo: nil, repeats: true)
+    }
+    @IBAction func stopTimer(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    @objc
+    func processTimer()
+    {
+        print("Timer triggered...")
+    }
+    
 }
 
