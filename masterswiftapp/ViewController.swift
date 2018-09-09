b7ffce08d20cc3531cd217ad13cba851d6d4da4b
mahmut <javaon@yahoo.com>
2018-09-09 08:01:02 +0300

    AVFoundation Sound File

--- masterswiftapp/ViewController.swift
+++ masterswiftapp/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    var audioPlayer: AVAudioPlayer!
+    let sounds = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func allButtonsAction(_ sender: UIButton)
+    {
+        var selectedSoundName: String = sounds[sender.tag - 1]
+
+        playSound(selectedSoundName)
+    }
+
+    func playSound(_ source: String)
+    {
+        let soundURL = Bundle.main.url(forResource: source, withExtension: "wav")
+
+        do
+        {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!);
+        } catch
+        {
+            print(error)
+        }
+
+        audioPlayer.play();
+    }
 
 }
 
